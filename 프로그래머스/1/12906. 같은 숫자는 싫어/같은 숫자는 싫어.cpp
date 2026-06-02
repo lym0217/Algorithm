3fa48ef9a3512ea696b0091be092a92daffd142f
Yeongmin_Lee <65957439+lym0217@users.noreply.github.com>
2026-06-02 22:10:31 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 85.76 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    queue<int> ans;
+    ans.push(arr[0]);
+    answer.push_back(arr[0]);
+    for(int i : arr){
+        if(ans.back() != i){
+            answer.push_back(i);
+            ans.push(i);
+        }
+    }
+    return (vector<int>)answer;
+}
